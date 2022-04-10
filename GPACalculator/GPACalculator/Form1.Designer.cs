21c5c13a0ab91e74a41e046bbef3f906cf817458
Pure227 <96216623+Pure227@users.noreply.github.com>
2022-04-11 01:45:08 +0700

    main

---
+++ GPACalculator/GPACalculator/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace GPACalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxGPA_input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxGPAx = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxminname = new System.Windows.Forms.TextBox();
+            this.textBoxmaxname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxmin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxmax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxname_input = new System.Windows.Forms.TextBox();
+            this.textBoxalldata = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GPA Input";
+            // 
+            // textBoxGPA_input
+            // 
+            this.textBoxGPA_input.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxGPA_input.Location = new System.Drawing.Point(418, 47);
+            this.textBoxGPA_input.Name = "textBoxGPA_input";
+            this.textBoxGPA_input.Size = new System.Drawing.Size(131, 61);
+            this.textBoxGPA_input.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(555, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add GPA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxGPAx
+            // 
+            this.textBoxGPAx.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxGPAx.Location = new System.Drawing.Point(163, 22);
+            this.textBoxGPAx.Name = "textBoxGPAx";
+            this.textBoxGPAx.Size = new System.Drawing.Size(131, 61);
+            this.textBoxGPAx.TabIndex = 3;
+            this.textBoxGPAx.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxminname);
+            this.groupBox1.Controls.Add(this.textBoxmaxname);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxmin);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxmax);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxGPAx);
+            this.groupBox1.Location = new System.Drawing.Point(12, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(681, 230);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // textBoxminname
+            // 
+            this.textBoxminname.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxminname.Location = new System.Drawing.Point(163, 156);
+            this.textBoxminname.Name = "textBoxminname";
+            this.textBoxminname.Size = new System.Drawing.Size(237, 61);
+            this.textBoxminname.TabIndex = 12;
+            this.textBoxminname.TabStop = false;
+            // 
+            // textBoxmaxname
+            // 
+            this.textBoxmaxname.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxmaxname.Location = new System.Drawing.Point(163, 89);
+            this.textBoxmaxname.Name = "textBoxmaxname";
+            this.textBoxmaxname.Size = new System.Drawing.Size(237, 61);
+            this.textBoxmaxname.TabIndex = 11;
+            this.textBoxmaxname.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "MIN GPAX";
+            // 
+            // textBoxmin
+            // 
+            this.textBoxmin.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxmin.Location = new System.Drawing.Point(406, 156);
+            this.textBoxmin.Name = "textBoxmin";
+            this.textBoxmin.Size = new System.Drawing.Size(131, 61);
+            this.textBoxmin.TabIndex = 8;
+            this.textBoxmin.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MAX GPAX";
+            // 
+            // textBoxmax
+            // 
+            this.textBoxmax.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxmax.Location = new System.Drawing.Point(406, 89);
+            this.textBoxmax.Name = "textBoxmax";
+            this.textBoxmax.Size = new System.Drawing.Size(131, 61);
+            this.textBoxmax.TabIndex = 6;
+            this.textBoxmax.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "GPAX";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(555, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxname_input
+            // 
+            this.textBoxname_input.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxname_input.Location = new System.Drawing.Point(175, 47);
+            this.textBoxname_input.Name = "textBoxname_input";
+            this.textBoxname_input.Size = new System.Drawing.Size(237, 61);
+            this.textBoxname_input.TabIndex = 1;
+            // 
+            // textBoxalldata
+            // 
+            this.textBoxalldata.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxalldata.Location = new System.Drawing.Point(12, 369);
+            this.textBoxalldata.Multiline = true;
+            this.textBoxalldata.Name = "textBoxalldata";
+            this.textBoxalldata.Size = new System.Drawing.Size(681, 213);
+            this.textBoxalldata.TabIndex = 13;
+            this.textBoxalldata.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(705, 594);
+            this.Controls.Add(this.textBoxalldata);
+            this.Controls.Add(this.textBoxname_input);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxGPA_input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxGPA_input;
+        private Button button1;
+        private TextBox textBoxGPAx;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Button button2;
+        private Label label4;
+        private TextBox textBoxmin;
+        private Label label3;
+        private TextBox textBoxmax;
+        private TextBox textBoxminname;
+        private TextBox textBoxmaxname;
+        private TextBox textBoxname_input;
+        private TextBox textBoxalldata;
+    }
+}
---
+++ GPACalculator/GPACalculator/GPACalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPACalculator
+{
+    internal class GPACalculator
+    {
+        private double sum = 0;
+        private int n = 0;
+        private double max = 0;
+        private double min = 4;
+        private string name = "";
+        private string name1 = "";
+        private string name2 = "";
+        private string alldata = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gpa"></param>
+        public void addGPA(double gpa,string name )
+        {
+            this.sum += gpa;
+            this.n++;
+            this.alldata += name + " " + gpa + Environment.NewLine;
+
+            if(this.max < gpa)
+            {
+                this.max = gpa;
+                this.name1 = name;
+            }
+            if (this.min > gpa)
+            {
+                this.min = gpa;
+                this.name2 = name;
+
+            }
+        }
+        public double getGPA()
+        {
+            double result = this.sum / this.n;
+            return result;
+
+        }
+        public double getmin()
+        {
+            return this.min;
+        }
+        public double getmax()
+        {
+            return this.max;
+        }
+        public string getmaxname()
+        {
+            return name1;
+        }
+        public string getalldata()
+        {
+            return alldata;
+        }
+        public string getminname()
+        {
+            return name2;
+        }
+    }
+}
